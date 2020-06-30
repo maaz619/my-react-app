e3bb73b785eeb6622c498f912178fc9dd09e0ae8
maaz619 <ahmedmaaz919@gmail.com>
2020-06-30 12:14:27 +0530

    initial commit

---
+++ src/components/home.jsx
@@ -0,0 +1,37 @@
+import React, {Component} from "react";
+import axios from "axios"
+import "./home.css"
+
+class Home extends Component{
+    constructor(props){
+        super(props)
+        this.state={quote:"LOADING........"}
+    }
+    async componentDidMount(){
+        const body= await axios.get("https://type.fit/api/quotes");
+        this.thedata=body.data
+        this.setState({
+            quote:this.thedata[Math.floor(Math.random()* this.thedata.length)].text
+        })
+    }
+    handleClick= async () =>{
+        this.setState({
+            quote:this.thedata[Math.floor(Math.random()* this.thedata.length)].text
+        })
+    }
+    render(){
+        return(
+        <div className="container">
+            <div className="quote-box">
+                <div className="quote-content">
+                    {this.state.quote}
+                </div>
+                <div className="button-refresh">
+                    <button class="refresher" onClick={this.handleClick}>GET NEW</button>
+                </div>
+            </div>
+        </div>
+        )
+    }
+}
+export default Home
