37da84857f97c9290eaf526b7df89e5257872ea0
maaz619 <ahmedmaaz919@gmail.com>
2020-07-01 14:22:12 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React,{Component} from "react";
-import "./App.css"
-import Home from "./components/home"
+import "./App.css";
+import Home from "./components/home";
+import Navbar from "./components/Navbar"
 
 class App extends Component{
     render(){
         return(
             <div className="app">
+                <Navbar />
                 <Home />
             </div>
         )
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+    return(
+        <div className="nav-bar">
+            <nav>
+                <div className="nav-bar-items">Home</div>
+                <div className="nav-bar-items">About</div>
+                <div className="nav-bar-items">Contact</div>
+                <div className="nav-bar-items">Login</div>
+            </nav>
+        </div>
+    )
+}
+export default Navbar
