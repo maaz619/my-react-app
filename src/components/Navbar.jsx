5d8a4221eac6a84458cd77bad7224f547d4911e3
Tanweer Anwar <tanweer.anwar919@gmail.com>
2020-07-01 22:23:02 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,8 @@ import "./Navbar.css";
 
 const Navbar = () => {
     return(
-        <div className="nav-bar">
-            <nav>
+        <div>
+            <nav className="nav-bar">
                 <div className="nav-bar-items">Home</div>
                 <div className="nav-bar-items">About</div>
                 <div className="nav-bar-items">Contact</div>
