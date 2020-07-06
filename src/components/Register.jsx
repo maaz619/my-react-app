8737e81d4f1ae24a290cde68ba68975ce3fbc08f
maaz619 <ahmedmaaz919@gmail.com>
2020-07-06 09:25:18 +0530

    updated

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -28,19 +28,19 @@ class Register extends Component {
                     <form onSubmit={this.handleSubmit}>
                         <div className="mb-3">
                             <label htmlFor="first_name" className="form-label">First Name</label>
-                            <input type="text" onChange={this.handleChange} className="form-control" name="first_name"/>
+                            <input type="text" id="first_name" onChange={this.handleChange} className="form-control" name="first_name"/>
                         </div>
                         <div className="mb-3">
                             <label htmlFor="last_name" className="form-label">Last Name</label>
-                            <input type="text" onChange={this.handleChange} className="form-control" name="last_name"/>
+                            <input type="text" id="last_name" onChange={this.handleChange} className="form-control" name="last_name"/>
                         </div>
                         <div className="mb-3">
-                            <label htmlFor="emai_name" className="form-label">Email</label>
-                            <input type="email" onChange={this.handleChange} className="form-control" name="email"/>
+                            <label htmlFor="email" className="form-label">Email</label>
+                            <input type="email" id="email" onChange={this.handleChange} className="form-control" name="email"/>
                         </div>
                         <div className="mb-3">
                             <label htmlFor="password" className="form-label">Password</label>
-                            <input type="password" onChange={this.handleChange} className="form-control" name="password"/>
+                            <input type="password" id="password" onChange={this.handleChange} className="form-control" name="password"/>
                         </div>
                         <button className="btn btn-primary">Register</button>
                     </form>
